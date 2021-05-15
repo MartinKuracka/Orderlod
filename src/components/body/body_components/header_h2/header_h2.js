3fdc8b3bc0c7bd59acbf228a26e6e7ada326b3b4
MartinKuracka <61198403+MartinKuracka@users.noreply.github.com>
2021-05-15 12:31:40 +0200

    styling of the header text

--- src/components/body/body_components/header_h2/header_h2.js
+++ src/components/body/body_components/header_h2/header_h2.js
@@ -1,9 +1,15 @@
 const H2 = (props) => {
     return (
-        <div className='flex h-10 my-4'>
-        <div className='h-full w-2 bg-header mr-5'></div>
-        <h2 className='text-2xl bg-gradient-to-r from-header2 to-header font-bold w-full pl-3'>{props.children}</h2>
+        <>
+        <div className='flex h-14 mt-4'>
+        <div className='h-full w-2 bg-header -mt-2'></div>
+        <div className='h-full w-1 bg-gray-800'></div>
+
+        <h2 className='text-black h-full text-2xl font-bold w-full bg-gradient-to-r	 from-gray-200  to-header2 pl-3 py-3'>{props.children}</h2>
+        
         </div>
+        <div className='w-full h-1 bg-gradient-to-r from-white  to-black mb-4'></div>
+        </>
     )
 }
 
